77da90c320ef6020a7e79681c17e20e5db72c451
nilimakhambalkar <vini1921>
2014-08-07 10:22:26 -0400

    Updated TODO

---
+++ public/main.js
@@ -0,0 +1,34 @@
+var todoapp = angular.module('todoApp',[]);
+todoapp.controller('todoController', function($scope,$http){
+	$scope.formData = [];
+
+	$http.get('/api/todos')
+		.success(function(data){
+			$scope.todos = data;
+		})
+		.error(function(data){
+			console.log('Error: ' + data);
+		});
+
+	$scope.createTodo = function(){
+		$http.post('/api/todos',$scope.formData)
+			.success(function(data){
+				$scope.formData = [];
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.deleteTodo = function(id){
+		$http.delete('/api/todos/'+id)
+			.success(function(data) {
+				$scope.todos = data;
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	}
+});
